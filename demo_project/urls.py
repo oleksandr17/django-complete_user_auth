134377154864ecdafe53206788a1dba8bac808ff
Oleksandr <oleksandr@elements.nl>
2018-12-23 14:07:30 +0100

    Add google auth

--- demo_project/urls.py
+++ demo_project/urls.py
@@ -19,7 +19,7 @@ from django.urls import include, path
 urlpatterns = [
     path('', include('pages.urls')),
     path('admin/', admin.site.urls),
-    path('users/', include('users.urls')),
-    path('users/', include('django.contrib.auth.urls')),
     path('accounts/', include('allauth.urls')),
+    # path('users/', include('users.urls')),
+    # path('users/', include('django.contrib.auth.urls')),
 ]
